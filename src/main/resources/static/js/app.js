35c25a9879a9ce2edab6b5457f52f709f85f0335
venflon1 <roberto.amato.11.93@gmail.com>
2019-12-02 14:46:40 +0000

    added folder js in static and files.js

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,18 @@
+// inietta i moduli ngRoute e ngResource nel mio modulo userregitrationsystem
+var app = angular.module('userregistrationsystem', ['ngRoute', 'ngResource']);
+
+app.config( function($routeProvider) {
+	$routeProvider.when('/list-all-users', {
+		templateUrl : '/template/listuser.html',
+		controller : 'listUserController'
+		}).when('/register-new-user',{
+		templateUrl : '/template/userregistration.html',
+		controller : 'registerUserController'
+		}).when('/update-user/:id',{
+		templateUrl : '/template/userupdation.html' ,
+		controller : 'usersDetailsController'
+		}).otherwise({
+			redirectTo : '/home',
+			templateUrl : '/template/home.html',
+			});
+});
